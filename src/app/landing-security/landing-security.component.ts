1eaed54030a5828f674309074a19ed0bde7aa61c
Max <max@maxleon.net>
2019-03-19 09:47:04 -0700

    - Updated cities on services pages. Removed unsearched terms North/East/South Bay and replaced with city names

--- src/app/landing-security/landing-security.component.ts
+++ src/app/landing-security/landing-security.component.ts
@@ -4,65 +4,67 @@ import { ActivatedRoute } from '@angular/router';
 import { ServicesService } from '../services/service.service';
 
 @Component({
-	selector: 'app-landing-security',
-	templateUrl: './landing-security.component.html',
-	styleUrls: ['./landing-security.component.sass'],
-	providers: [ServicesService]
+  selector: 'app-landing-security',
+  templateUrl: './landing-security.component.html',
+  styleUrls: ['./landing-security.component.sass'],
+  providers: [ServicesService]
 })
 export class LandingSecurityComponent implements OnInit {
 
-	private req: any
-	serviceList: [ServiceItem]
-	private routeSub: any;
-	slug: string;
-	title = "Security doors and window security for "
-	images:{};
-	cities: [string];
-	region: ServiceItem;
-	constructor(private route: ActivatedRoute, private _service: ServicesService) { }
-	regionList = {
-		"Sacramento": ["Antelope", "Arden Arcade", "Brooks", "Cameron Park", "Capay", "Carbondale", "Carmichael", "Citrus Heights", "Clarksburg", "Clay", "Conaway", "Courtland", "Davis", "El Dorado Hills", "El Macero", "Elk Grove", "Elverta", "Esparto", "Fair Oaks", "Florin", "Folsom", "Foothill Farms", "Granite Bay", "Knights Landing", "Lincoln", "Loomis", "Madison", "Monument Hills", "Newcastle", "Penryn", "Plainfield", "Sacramento", "West Sacramento", "Woodland", "Yolo", "Zamora"],
-		"Auburn": ["Amador City", "Applegate", "Auburn", "Brooks", "Bunker Hill", "Cameron Park", "Coloma", "Cool", "Drytown", "Dunnigan", "Fiddletown", "Guinda", "Ione", "Jackson", "Martell", "Meadow Vista", "New Chicago", "North Auburn", "Pilot Hill", "Placerville", "Plymouth", "Rio Oso", "River Pines", "Rumsey", "Sheridan", "Sunnybrook", "Sutter Creek", "Sutter Hill", "Wheatland"],
-		"Antioch and the East Bay Region": ["Bay Point", "Bethel Island", "Brentwood", "Byron", "Clayton", "Contra Costa Centre", "Discovery Bay", "Knightsen", "Oakley", "Pittsburg", "Shell Ridge"],
-		"Stockton": ["Acampo", "Adela", "Airport", "Allen", "Atlanta", "August", "Banta", "Barton", "Bonnefoy", "Buena Vista", "North Shore Lake Camanche", "Camanche Village", "Claribel", "Clarsona", "Claus", "Clay", "Clinton", "Collierville", "Courtland", "Dagon", "Del Rio", "Dogtown", "East Oakdale", "Edwin", "Electra", "Escalon", "Eugene", "Farmington", "Firebrick", "French Camp", "Garden Acres", "Goodmans Corner", "Kennedy", "Kirkwood", "Kit Carson", "Knights Ferry", "Langworth", "Lathrop", "Lincoln Village", "Linden", "Lockeford", "Lodi", "Manteca", "McHenry", "Morada", "Mormon", "Mountain House", "Oakdale", "Peters", "Pine Acres", "Pine Grove", "Pioneer", "Plasse", "Red Corral", "Ripon", "Riverbank", "Salida", "Scottsville", "Stockton", "Taft Mosswood", "Thornton", "Tracy", "Valley Home", "Victor", "Volcano", "Waterloo", "Woodbridge"],
-		"South Bay": ["Acalanes Ridge", "Alamo", "Ashland", "Blackhawk", "Camino Tassajara", "Castro Valley", "Castle Hill", "Danville", "Diablo", "Dublin", "Fremont", "Hayward", "Livermore", "Moraga", "Newark", "Norris Canyon", "Pleasanton", "San Leandro", "San Lorenzo", "San Ramon", "Sunol", "Walnut Creek"],
-		"Oakland": ["Alameda", "Alhambra Valley", "Albany", "Beyview-Mountain", "Berkeley", "Canyon", "Clyde", "Concord", "Crockett", "East Richmond Heights", "El Cerrito", "El Sobrante", " Emeryville", "Hasford Heights", "Hercules", "Kensington", "Lafayette", "Martinez", "North Richmond", "Orinda", "Pacheco", "Piedmont", "Pinole", "Pleasant Hill", "Port Costa", "Reliez Valley", "Richmond", "Rodeo", "Rollingwood", "San Pablo", "Tara Hills", "Vine Hill"],
-		"Modesto": ["Atwater", "Bret Harte", "Bystrom", "Ceres", "Crows Landing", "Diablo Grande", "Denair", "Empire", "Grayson", "Hickman", "Hills Ferry", "Keys", "Hughson", "Livingston", "La Grange", "Los Banos", "Merced", "Modesto", "Monterey Park Tract", "Montpelier", "New Jerusalem", "Newman", "Orestimba High School", "Patterson", "Riverdale Park", "Roberts Ferry Grammer School", "Shackelford", "Turlock", "Vernalis", "Waterford", "Westley", "West Modesto"],
-		"Napa": ["American Canyon", "Atlas", "Batoo", "Benicia", "Imola", "Napa", "Oak Knoll", "Schellville", "Sonoma", "Union", "Vallejo", "Vineburg", "Yountville"],
-		"Vacaville": ["Allendale", "Bahia", "Birds Landing", "Bucktown", "Collinsville", "Cordelia", "Dixon", "Elmira", "Fairfield", "Green Valley", "Hartley", "Maine Prairie", "Rio Vista", "Rockville", "Terminous", "Suisun City", "Vacaville", "Scandia Family Center", "Winters"]
-	}
-	regionNumbers={
-		"Sacramento":"(916)823-3388",
-		"Auburn":"(530)692-5680",
-		"Antioch and the East Bay Region":"(925) 350-0501",
-		"Stockton":"(209)815-6085",
-		"South Bay":"(925)443-0700",
-		"Oakland":"(510)277-5694",
-		"Modesto":"(209)957-9700",
-		"Napa":"(707) 812-1935",
-		"Vacaville":"(707)542-0626"
-	}
-	ngOnInit() {
-		this.routeSub = this.route.params.subscribe(params => {
-			this.slug = params['slug']
-			this.req = this._service.list().subscribe(data => {
-				data.filter(item => {
-					if (item.slug == this.slug && item.type=='landing-security') {
-						this.region = item as ServiceItem;
-						//console.log(this.region)
-						this.cities = this.regionList[this.region.region]
-						this.title += this.region.region
-						if(this.region.hasOwnProperty('images'))
-						this.images=this.region['images'];
-						//console.log(this.cities)
-					}
-				}
-				)
-			})
-		})
-	}
+  private req: any
+  serviceList: [ServiceItem]
+  private routeSub: any;
+  slug: string;
+  title = "Security doors and window security for "
+  images:{};
+  cities: [string];
+  region: ServiceItem;
 
-	ngOnDestroy() {
-		this.req.unsubscribe();
-	}
+  constructor(private route: ActivatedRoute, private _service: ServicesService) { }
+
+  regionList = {
+    "Sacramento": ["Antelope", "Arden Arcade", "Brooks", "Cameron Park", "Capay", "Carbondale", "Carmichael", "Citrus Heights", "Clarksburg", "Clay", "Conaway", "Courtland", "Davis", "El Dorado Hills", "El Macero", "Elk Grove", "Elverta", "Esparto", "Fair Oaks", "Florin", "Folsom", "Foothill Farms", "Granite Bay", "Knights Landing", "Lincoln", "Loomis", "Madison", "Monument Hills", "Newcastle", "Penryn", "Plainfield", "Sacramento", "West Sacramento", "Woodland", "Yolo", "Zamora"],
+    "Auburn": ["Amador City", "Applegate", "Auburn", "Brooks", "Bunker Hill", "Cameron Park", "Coloma", "Cool", "Drytown", "Dunnigan", "Fiddletown", "Guinda", "Ione", "Jackson", "Martell", "Meadow Vista", "New Chicago", "North Auburn", "Pilot Hill", "Placerville", "Plymouth", "Rio Oso", "River Pines", "Rumsey", "Sheridan", "Sunnybrook", "Sutter Creek", "Sutter Hill", "Wheatland"],
+    "Antioch": ["Bay Point", "Bethel Island", "Brentwood", "Byron", "Clayton", "Contra Costa Centre", "Discovery Bay", "Knightsen", "Oakley", "Pittsburg", "Shell Ridge"],
+    "Stockton": ["Acampo", "Adela", "Airport", "Allen", "Atlanta", "August", "Banta", "Barton", "Bonnefoy", "Buena Vista", "North Shore Lake Camanche", "Camanche Village", "Claribel", "Clarsona", "Claus", "Clay", "Clinton", "Collierville", "Courtland", "Dagon", "Del Rio", "Dogtown", "East Oakdale", "Edwin", "Electra", "Escalon", "Eugene", "Farmington", "Firebrick", "French Camp", "Garden Acres", "Goodmans Corner", "Kennedy", "Kirkwood", "Kit Carson", "Knights Ferry", "Langworth", "Lathrop", "Lincoln Village", "Linden", "Lockeford", "Lodi", "Manteca", "McHenry", "Morada", "Mormon", "Mountain House", "Oakdale", "Peters", "Pine Acres", "Pine Grove", "Pioneer", "Plasse", "Red Corral", "Ripon", "Riverbank", "Salida", "Scottsville", "Stockton", "Taft Mosswood", "Thornton", "Tracy", "Valley Home", "Victor", "Volcano", "Waterloo", "Woodbridge"],
+    "Walnut Creek": ["Acalanes Ridge", "Alamo", "Ashland", "Blackhawk", "Camino Tassajara", "Castro Valley", "Castle Hill", "Danville", "Diablo", "Dublin", "Fremont", "Hayward", "Livermore", "Moraga", "Newark", "Norris Canyon", "Pleasanton", "San Leandro", "San Lorenzo", "San Ramon", "Sunol", "Walnut Creek"],
+    "Oakland": ["Alameda", "Alhambra Valley", "Albany", "Beyview-Mountain", "Berkeley", "Canyon", "Clyde", "Concord", "Crockett", "East Richmond Heights", "El Cerrito", "El Sobrante", " Emeryville", "Hasford Heights", "Hercules", "Kensington", "Lafayette", "Martinez", "North Richmond", "Orinda", "Pacheco", "Piedmont", "Pinole", "Pleasant Hill", "Port Costa", "Reliez Valley", "Richmond", "Rodeo", "Rollingwood", "San Pablo", "Tara Hills", "Vine Hill"],
+    "Modesto": ["Atwater", "Bret Harte", "Bystrom", "Ceres", "Crows Landing", "Diablo Grande", "Denair", "Empire", "Grayson", "Hickman", "Hills Ferry", "Keys", "Hughson", "Livingston", "La Grange", "Los Banos", "Merced", "Modesto", "Monterey Park Tract", "Montpelier", "New Jerusalem", "Newman", "Orestimba High School", "Patterson", "Riverdale Park", "Roberts Ferry Grammer School", "Shackelford", "Turlock", "Vernalis", "Waterford", "Westley", "West Modesto"],
+    "Napa": ["American Canyon", "Atlas", "Batoo", "Benicia", "Imola", "Napa", "Oak Knoll", "Schellville", "Sonoma", "Union", "Vallejo", "Vineburg", "Yountville"],
+    "Vacaville": ["Allendale", "Bahia", "Birds Landing", "Bucktown", "Collinsville", "Cordelia", "Dixon", "Elmira", "Fairfield", "Green Valley", "Hartley", "Maine Prairie", "Rio Vista", "Rockville", "Terminous", "Suisun City", "Vacaville", "Scandia Family Center", "Winters"]
+  }
+
+  regionNumbers = {
+    "Sacramento":"(916) 823-3388",
+    "Auburn":"(530) 692-5680",
+    "Antioch":"(925) 350-0501",
+    "Stockton":"(209) 815-6085",
+    "Walnut Creek":"(925) 443-0700",
+    "Oakland":"(510) 277-5694",
+    "Modesto":"(209) 957-9700",
+    "Napa":"(707) 812-1935",
+    "Vacaville":"(707) 542-0626"
+  }
+
+  ngOnInit() {
+    this.routeSub = this.route.params.subscribe((params) => {
+      this.slug = params['slug']
+      this.req = this._service.list().subscribe((data) => {
+        data.filter((item) => {
+          if (item.slug == this.slug && item.type=='landing-security') {
+            this.region = item as ServiceItem;
+            this.cities = this.regionList[this.region.region]
+            this.title += this.region.region
+            if (this.region.hasOwnProperty('images')) {
+              this.images=this.region['images']
+            }
+          }
+        })
+      })
+    })
+  }
+
+  ngOnDestroy() {
+    this.req.unsubscribe();
+  }
 }
--- src/app/region-list/region-list.component.ts
+++ src/app/region-list/region-list.component.ts
@@ -15,8 +15,6 @@ export class RegionListComponent implements OnInit, OnDestroy {
   regionList: [Region]
   regions = [];
 
-
-  // Remove the duplicate Categories
   onlyUnique(value, index, self) {
     return self.indexOf(value) === index;
   }
@@ -24,38 +22,16 @@ export class RegionListComponent implements OnInit, OnDestroy {
   constructor(private _service: RegionsService) { }
 
   ngOnInit() {
-    this.req = this._service.list().subscribe(data => {
+    this.req = this._service.list().subscribe((data) => {
       this.regionList = data as [Region];
-      //console.log("regionList: ",this.regionList);
-      // The Category Filter Dropdown
       for (var i = 0; i < this.regionList.length; i++) {
         this.regions.push(this.regionList[i].region)
       }
-      // Don't duplicate Categories
       this.regions = this.regions.filter(this.onlyUnique)
-      // console.log(this.category)
-      //console.log("Regions:",this.regions)
     })
-    
   }
 
   ngOnDestroy() {
     this.req.unsubscribe();
   }
-
 }
-// import { Component, OnInit } from '@angular/core';
-
-// @Component({
-//   selector: 'app-region-list',
-//   templateUrl: './region-list.component.html',
-//   styleUrls: ['./region-list.component.sass']
-// })
-// export class RegionListComponent implements OnInit {
-
-//   constructor() { }
-
-//   ngOnInit() {
-//   }
-
-// }
