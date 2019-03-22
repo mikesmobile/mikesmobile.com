3896f5681e575e73f074467605d4300d849e9195
Max <max@maxleon.net>
2019-03-22 13:29:26 -0700

    - Added Titan Security Door products - Changed links to Steel Security Doors to Titan

--- src/app/color-swatch/color-swatch.component.ts
+++ src/app/color-swatch/color-swatch.component.ts
@@ -5,17 +5,20 @@ import { Component, OnInit, Input } from '@angular/core';
   templateUrl: './color-swatch.component.html',
   styleUrls: ['./color-swatch.component.sass']
 })
+
 export class ColorSwatchComponent implements OnInit {
   @Input() swatchType;
   colors:{};
+
   stdColors = {
     "name":"Standard Colors",
     "values":{
     "Anodized Bronze":"#2D3017",
     "White":"#ffffff"
     }
-}
-colorSetx4 = {
+  }
+
+  colorSetx4 = {
     "name":"Frame Colors",
     "values":{
       "Almond":"#F3E7D3",
@@ -24,7 +27,8 @@ colorSetx4 = {
       "White":"#ffffff"
     }
   }
-colorSetx6 = {
+
+  colorSetx6 = {
     "name":"Custom Colors",
     "values":{
       "Black":"#000000",
@@ -35,7 +39,8 @@ colorSetx6 = {
       "White":"#ffffff"
     }
   }
-swingingDoorColors = {
+
+  swingingDoorColors = {
     "name":"Custom Colors",
     "values":{
       "Adobe":"#E0B17D",
@@ -48,7 +53,8 @@ swingingDoorColors = {
       "White":"#ffffff"
     }
   }
-slidingSecurityDoorColors ={ 
+
+  slidingSecurityDoorColors ={
     "name":"Custom Colors",
     "values":{
       "Alabaster":"#FAF9ED",
@@ -70,17 +76,19 @@ slidingSecurityDoorColors ={
       "Pacific Granite":"#707070",
       "Forest Green":"#0E251B",
       "Statuary Bronze":"#42372F"
-      }
     }
-    screenFrameColors = {
+  }
+
+  screenFrameColors = {
     "name":"Screen Colors",
     "values":{
       Black:"/assets/images/colors/suntex-black.png",
       Brown:"/assets/images/colors/suntex-brown.png",
       Gray:"/assets/images/colors/suntex-grey.png",
       Stucco:"/assets/images/colors/suntex-stucco.png"
+    }
   }
-}
+
   petDoorColors = {
     "name":"Pet Door Frames",
     "values":{
@@ -90,6 +98,7 @@ slidingSecurityDoorColors ={
       "White":"/assets/images/screens/pet-door/white-frame.jpg"
     }
   }
+
   quickEscapeColors = {
     "name":"Custom Colors",
     "values":{
@@ -102,65 +111,102 @@ slidingSecurityDoorColors ={
       "Sahara Gray":"#92989b",
       "Silver":"#e9e9eb",
       "White":"#ffffff"
-      }
+    }
   }
-  constructor() { 
 
-    
+  titanSecurityColors = {
+    "name":"Frame Colors",
+    "values":{
+      "White":"/assets/images/security/titan/colors/white.jpg",
+      "Beige Hammer":"/assets/images/security/titan/colors/beige-hammer.jpg",
+      "Desert Sand":"/assets/images/security/titan/colors/desert-sand.jpg",
+      "Terra Beige":"/assets/images/security/titan/colors/terra-beige.jpg",
+      "Silverado":"/assets/images/security/titan/colors/silverado.jpg",
+      "Royal Brown":"/assets/images/security/titan/colors/royal-brown.jpg",
+      "Copperclad":"/assets/images/security/titan/colors/copperclad.jpg",
+      "Wineberry":"/assets/images/security/titan/colors/wineberry.jpg",
+      "Black":"/assets/images/security/titan/colors/black.jpg",
+      "Blue Hammertone":"/assets/images/security/titan/colors/blue-hammertone.jpg",
+      "Forest Green":"/assets/images/security/titan/colors/forest-green.jpg",
+      "Red Hammertone":"/assets/images/security/titan/colors/red-hammertone.jpg",
+    }
   }
+
+  titanAluminumColors = {
+    "name":"Aluminum Security Screen Colors",
+    "values":{
+      "White":"/assets/images/security/titan/colors/aluminum_white.jpg",
+      "Beige Hammer":"/assets/images/security/titan/colors/aluminum_beige-hammer.jpg",
+      "Desert Sand":"/assets/images/security/titan/colors/aluminum_desert-sand.jpg",
+      "Royal Brown":"/assets/images/security/titan/colors/aluminum_royal-brown.jpg",
+      "Black":"/assets/images/security/titan/colors/aluminum_black.jpg",
+    }
+  }
+
+  constructor() {}
 
   ngOnInit() {
-    //console.log(this.swatchType,this.swatchType==="colorSetx4",this.colorSetx4)
-    switch(this.swatchType){
+    switch(this.swatchType) {
       case "stdColors":
-        this.colors=this.stdColors;
-        break;
+        this.colors = this.stdColors
+        break
       case "colorSetx4":
-        this.colors=this.colorSetx4;
-        break;
+        this.colors = this.colorSetx4
+        break
       case "colorSetx6":
-          this.colors=this.colorSetx6;
-          break;
+          this.colors = this.colorSetx6
+          break
       case "swingingDoorColors":
-          this.colors=this.swingingDoorColors;
-          break;
+          this.colors = this.swingingDoorColors
+          break
       case "slidingSecurityDoorColors":
-          this.colors=this.slidingSecurityDoorColors;
-          break;
+          this.colors = this.slidingSecurityDoorColors
+          break
       case "screenFrameColors":
-          this.colors=this.screenFrameColors;
-          break;
+          this.colors = this.screenFrameColors
+          break
       case "petDoorColors":
-        this.colors=this.petDoorColors;
-        break;
+        this.colors = this.petDoorColors
+        break
       case "quickEscapeColors":
-        this.colors=this.quickEscapeColors;
-        break;
+        this.colors = this.quickEscapeColors
+        break
+      case "titanSecurityColors":
+        this.colors = this.titanSecurityColors
+        break
+      case "titanAluminumColors":
+        this.colors = this.titanAluminumColors
+        break
       default:
-        this.colors=null;
-        break;
+        this.colors = null;
+        break
       }
   }
+
   viewMoreColors(){
-    var viewMoreBtn = document.getElementById('more-colors').style.display="none";
-    var viewLessBtn = document.getElementById('less-colors').style.display="block";
-
-    for (var i=6; i<19; i++){
-        var el = document.getElementById("square-" + i);            
-        el.style.display = 'block';            
+    document.getElementById('more-colors').style.display = "none"
+    document.getElementById('less-colors').style.display = "block"
+    for (let i = 6; i < 19; i++) {
+      let el = document.getElementById(`square-${i}`)
+      if (el) {
+        el.style.display = 'block'
+      }
     }
-    
   }
-  viewLessColors(){
-      var viewLessBtn = document.getElementById('less-colors').style.display="none";
-      var viewMoreBtn = document.getElementById('more-colors').style.display="block";
 
-      for (var i=6; i<19; i++){
-          var el = document.getElementById("square-" + i);            
-          el.style.display = 'none';          
-      }     
+  viewLessColors() {
+    document.getElementById('less-colors').style.display = "none"
+    document.getElementById('more-colors').style.display = "block"
+
+    for (let i = 6; i < 19; i++) {
+      let el = document.getElementById(`square-${i}`)
+      if (el) {
+        el.style.display = 'none'
+      }
+    }
   }
-  moreSteelToggle(){
-    var collapse = document.getElementById('collapseSteelColors').classList.toggle('collapse')
-}
+
+  moreSteelToggle() {
+    document.getElementById('collapseSteelColors').classList.toggle('collapse')
+  }
 }
--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit, OnDestroy, ViewChild, ViewEncapsulation } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ServiceItem } from '../services/service';
-import { ServicesService } from '../services/service.service';
-import { DomSanitizer } from '@angular/platform-browser';
-import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation, NgxGalleryImageSize } from 'ngx-gallery';
+import { Component, OnInit,
+        OnDestroy, ViewChild,
+        ViewEncapsulation } from '@angular/core';
+import { DomSanitizer }     from '@angular/platform-browser';
+import { ActivatedRoute }   from '@angular/router';
+import { NgxGalleryOptions,
+        NgxGalleryImage,
+        NgxGalleryAnimation,
+        NgxGalleryImageSize } from 'ngx-gallery';
 
+import { ServiceItem }        from '../services/service';
+import { ServicesService }    from '../services/service.service';
 import { QuoteFormComponent } from '../quote-form/quote-form.component';
-import {TabNavComponent} from '../tab-nav/tab-nav.component';
+import { TabNavComponent }    from '../tab-nav/tab-nav.component';
+
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
@@ -14,244 +20,127 @@ import {TabNavComponent} from '../tab-nav/tab-nav.component';
   providers: [ServicesService],
   encapsulation: ViewEncapsulation.None
 })
+
 export class ProductDetailComponent implements OnInit, OnDestroy {
+  gallery_options: NgxGalleryOptions[]
+  gallery_images: NgxGalleryImage[]
+  recent_install_images_options: NgxGalleryOptions[]
+  recent_install_images:NgxGalleryImage[]
+  details:any
 
-    gallery_options: NgxGalleryOptions[];
-    gallery_images: NgxGalleryImage[];
-    recent_install_images_options: NgxGalleryOptions[];
-    recent_install_images:NgxGalleryImage[];
-    details:any
-    groupOptionsSelect=[{}]
+  private req:any
+  private routeSub:any
 
-    private req:any;
-    private routeSub:any;
+  slug:string
+  product:ServiceItem
+  videoLink=null
 
-    slug:string;
-    product:ServiceItem;
-    videoLink=null
-    colorList = ['stdColors', 'colorSetx4', 'colorSetx6', 'swingingDoorColors', 'securityDoorColors'];
+  @ViewChild(QuoteFormComponent) private quoteForm:QuoteFormComponent
+  @ViewChild('navwrap') private navwrap
 
-    stdColors = {
-        "Anodized Bronze":"#2D3017",
-        "White":"#ffffff"
-    }
-    colorSetx4 = {
-        "Almond":"#F3E7D3",
-        "Anodized Bronze":"#2D3017",
-        "Mill":"#999AA3",
-        "White":"#ffffff"
-    }
-    colorSetx6 = {
-        "Black":"#000000",
-        "Bronze":"#473428",
-        "Champagne":"#A69882",
-        "Desert Sand":"#F4EBD4",
-        "Mill":"#999AA3",
-        "White":"#ffffff"
-    }
-    swingingDoorColors = {
-        "Adobe":"#E0B17D",
-        "Almond":"#F3E7D3",
-        "Black":"#000000",
-        "Anodized Bronze":"#2D3017",
-        "Mill":"#999AA3",
-        "Anodized Satin":"#E1E2E8",
-        "Tan":"#AD8166",
-        "White":"#ffffff"
-    }
-    slidingSecurityDoorColors = {
-        "Alabaster":"#FAF9ED",
-        "Almond":"#F4EDCA",
-        "Autumn Brown":"#3F2823",
-        "Bear Green":"#103427",
-        "Champagne Beige":"#928A6F",
-        "Charcoal Grey":"#535353",
-        "Country Blue":"#6C99B6",
-        "Desert Sand":"#E5E0B5",
-        "Flat Black":"#121412",
-        "New England Grey":"#B0BBB7",
-        "Post Office Blue":"#182F4D",
-        "Terra Cotta":"#671218",
-        "Evening Blue":"#1F4769",
-        "Chrome":"#EFEFEF",
-        "Green Patina":"#435E57",
-        "New Bronze":"#40372D",
-        "Pacific Granite":"#707070",
-        "Forest Green":"#0E251B",
-        "Statuary Bronze":"#42372F"
-    }
-    @ViewChild(QuoteFormComponent) private quoteForm:QuoteFormComponent;
-    toggleQuoteForm(){
-      this.quoteForm.show()
-    }
-    @ViewChild('navwrap') private navwrap;
+  toggleQuoteForm() {
+    this.quoteForm.show()
+  }
 
-    constructor(private route: ActivatedRoute, private _service:ServicesService, private sanitizer:DomSanitizer) { }
+  constructor(private route: ActivatedRoute, private _service:ServicesService, private sanitizer:DomSanitizer) { }
 
-    ngOnInit() {
-        this.routeSub = this.route.params.subscribe(params => {
-            this.slug = params['slug']
-            this.req = this._service.list().subscribe(data => {
-                data.filter(item => {
-                    if(item.slug == this.slug){
-                        this.product = item as ServiceItem;
-                        this.videoLink=this.sanitizer.bypassSecurityTrustUrl(this.product.video)
-                        this.recent_install_images = this.product.recentInstallImages;
-                        if(this.details != undefined){
-                            this.details = undefined
-                        }
-                        this.details=this.product.details
-                        if (this.product.thumbImages && this.product.thumbImages.length > 1){
-                            this.gallery_images = this.product.thumbImages;
-                            let galleryImages = true;
-                        } else {
-                            let modalImage = true;
-                        }
+  ngOnInit() {
+    this.routeSub = this.route.params.subscribe((params) => {
+      this.slug = params['slug']
+      this.req = this._service.list().subscribe((data) => {
+        data.filter((item) => {
+          if (item.slug == this.slug) {
+            this.product = item as ServiceItem;
+            this.videoLink = this.sanitizer.bypassSecurityTrustUrl(this.product.video)
+            this.recent_install_images = this.product.recentInstallImages;
 
-                        if (this.product.bullets!=undefined &&this.product.bullets.length > 5){
-                            let smallLi = true;
-                        }
-
-                    }
-                })
-            })
-        })
-
-        this.groupOptionsSelect = [
-            { value: '', label: 'DOOR AND WINDOW SCREENS', group: true },
-            { value: '1', label: 'Retractable Screen Doors' },
-            { value: '2', label: 'Sliding Screen Doors' },
-            { value: '3', label: 'Swinging Screen Doors' },
-            { value: '4', label: 'Pet Doors' },
-            { value: '5', label: 'Window Screen Repair' },
-            { value: '6', label: 'Solar Screens' },
-            { value: '', label: 'CHIMNEY SERVICES', group: true },
-            { value: '7', label: 'Chimney Inspection' },
-            { value: '8', label: 'Chimney Cleaning' },
-            { value: '9', label: 'Chimney Repair' },
-            { value: '10', label: 'Masonry Services' },
-            { value: '11', label: 'Dryer Vent Cleaning' },
-            { value: '', label: 'SECURITY DOORS AND WINDOWS', group: true },
-            { value: '12', label: 'Steel Security Doors' },
-            { value: '13', label: 'Viewguard' },
-            { value: '14', label: 'Tru-View' },
-            { value: '15', label: 'Tru-Frame' },
-            { value: '16', label: 'Sliding Security Doors' },
-            { value: '17', label: 'CRL Guarda Quick Escape' },
-            { value: '18', label: 'CRL Guarda Fixed' },
-            //{ value: '19', label: 'CRL Guarda Casement' },
-            { value: '', label: 'AWNINGS', group: true },
-            { value: '20', label: 'Retractable Patio Awnings' },
-            { value: '21', label: 'Drop Roll Sunscreens' },
-
-        ];
-
-        this.gallery_options = [
-            {
-                width: '550px',
-                height: '625px',
-                thumbnailSize: NgxGalleryImageSize.Cover,
-                thumbnailsColumns: 4,
-                thumbnailsRemainingCount: true,
-                imageArrowsAutoHide: true,
-                imageDescription: true,
-                thumbnailsArrowsAutoHide: true,
-                thumbnailsSwipe: true,
-                imageSize: NgxGalleryImageSize.Contain,
-                imageAnimation: NgxGalleryAnimation.Zoom,
-                previewCloseOnClick: true,
-                previewCloseOnEsc: true,
-                imageSwipe: true
-            },
-            // max-width 800
-            {
-                breakpoint: 800,
-                width: '100%',
-                height: '600px',
-                // imagePercent: 80,
-                thumbnailSize: NgxGalleryImageSize.Cover,
-                imageArrowsAutoHide: false,
-                thumbnailsPercent: 20,
-                thumbnailsMargin: 20,
-                thumbnailMargin: 20
-            },
-            // max-width 400
-            {
-                breakpoint: 400,
-                imageSwipe: true
+            if (this.details != undefined) {
+              this.details = undefined
             }
-        ];
 
-        this.recent_install_images_options = [
-            {
-                image: false,
-                height: '200px',
-                width: '800px',
-                // width: '600px',
-                // height: '500px',
-                thumbnailsColumns: 5,
-                // thumbnailsRows: 2,
-                // thumbnailsPercent: 40,
-                // imagePercent: 60,
-                // thumbnailMargin: 2,
-                // thumbnailsMargin: 2,
-                imageAutoPlay: true,
-                thumbnailsSwipe: true,
-                imageAutoPlayPauseOnHover: true,
-                previewAutoPlay: true,
-                previewAutoPlayPauseOnHover: true,
-                previewCloseOnClick: true,
-                previewCloseOnEsc: true
-            },
-            {
-              breakpoint: 990,
-              width: "100%",
-              thumbnailsColumns: 3,
+            this.details = this.product.details
 
-            },
-            // {
-            //     breakpoint: 300,
-            //     width: '100%',
-            //     height: '200px',
-            //     thumbnailsColumns: 2
-            // }
-        ]
-    }
-    ngOnDestroy(){
-      this.routeSub.unsubscribe();
-      this.req.unsubscribe();
-    }
+            if (this.product.thumbImages && this.product.thumbImages.length > 1) {
+              this.gallery_images = this.product.thumbImages;
+            }
+          }
+        })
+      })
+    })
 
-    // viewMoreColors(){
-    //     var viewMoreBtn = document.getElementById('more-colors').style.display="none";
-    //     var viewLessBtn = document.getElementById('less-colors').style.display="block";
+    this.gallery_options = [
+      {
+        width: '550px',
+        height: '625px',
+        thumbnailSize: NgxGalleryImageSize.Cover,
+        thumbnailsColumns: 4,
+        thumbnailsRemainingCount: true,
+        imageArrowsAutoHide: true,
+        imageDescription: true,
+        thumbnailsArrowsAutoHide: true,
+        thumbnailsSwipe: true,
+        imageSize: NgxGalleryImageSize.Contain,
+        imageAnimation: NgxGalleryAnimation.Zoom,
+        previewCloseOnClick: true,
+        previewCloseOnEsc: true,
+        imageSwipe: true
+      },
+      // max-width 800
+      {
+        breakpoint: 800,
+        width: '100%',
+        height: '600px',
+        // imagePercent: 80,
+        thumbnailSize: NgxGalleryImageSize.Cover,
+        imageArrowsAutoHide: false,
+        thumbnailsPercent: 20,
+        thumbnailsMargin: 20,
+        thumbnailMargin: 20
+      },
+      // max-width 400
+      {
+        breakpoint: 400,
+        imageSwipe: true
+      }
+    ];
 
-    //     for (var i=6; i<19; i++){
-    //         var el = document.getElementById("square-" + i);
-    //         el.style.display = 'block';
-    //         if (screen.width < 992){
-    //             var ello = document.getElementById("square-p-" + i);
-    //             ello.style.display = 'block';
-    //         }
-    //     }
+    this.recent_install_images_options = [
+      {
+        image: false,
+        height: '200px',
+        width: '800px',
+        // width: '600px',
+        // height: '500px',
+        thumbnailsColumns: 5,
+        // thumbnailsRows: 2,
+        // thumbnailsPercent: 40,
+        // imagePercent: 60,
+        // thumbnailMargin: 2,
+        // thumbnailsMargin: 2,
+        imageAutoPlay: true,
+        thumbnailsSwipe: true,
+        imageAutoPlayPauseOnHover: true,
+        previewAutoPlay: true,
+        previewAutoPlayPauseOnHover: true,
+        previewCloseOnClick: true,
+        previewCloseOnEsc: true
+      },
+      {
+        breakpoint: 990,
+        width: "100%",
+        thumbnailsColumns: 3,
 
-    // }
-    // viewLessColors(){
-    //     var viewLessBtn = document.getElementById('less-colors').style.display="none";
-    //     var viewMoreBtn = document.getElementById('more-colors').style.display="block";
-
-    //     for (var i=6; i<19; i++){
-    //         var el = document.getElementById("square-" + i);
-    //         el.style.display = 'none';
-    //         if (screen.width < 992){
-    //             var ello = document.getElementById("square-p-" + i);
-    //             ello.style.display = 'none';
-    //         }
-    //     }
-    // }
-
-    // moreSteelToggle(){
-    //     var collapse = document.getElementById('collapseSteelColors').classList.toggle('collapse')
-    // }
-
+      },
+      // {
+      //   breakpoint: 300,
+      //   width: '100%',
+      //   height: '200px',
+      //   thumbnailsColumns: 2
+      // }
+    ]
+  }
+  ngOnDestroy() {
+    this.routeSub.unsubscribe();
+    this.req.unsubscribe();
+  }
 }
