795d7e989e493ca18ab2a6cedd5ac56cd9e66268
Max <max@maxleon.net>
2019-03-26 15:40:05 -0700

    - Repaired broken search results for "resource" services - Code cleaning

--- src/app/search-detail/search-detail.component.ts
+++ src/app/search-detail/search-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ServiceItem } from '../services/service';
-import { ServicesService } from '../services/service.service';
+import { ActivatedRoute }               from '@angular/router';
+import { ServiceItem }      from '../services/service';
+import { ServicesService }  from '../services/service.service';
 
 @Component({
   selector: 'app-search-detail',
@@ -9,28 +9,33 @@ import { ServicesService } from '../services/service.service';
   styleUrls: ['./search-detail.component.sass'],
   providers: [ServicesService]
 })
+
 export class SearchDetailComponent implements OnInit, OnDestroy {
   private routeSub:any
   private req:any
   query:string
   searchList:[ServiceItem]
-  categories = ['Screens', 'Chimney', 'Security', 'Awnings']
-  categoryList=[]
 
   constructor(private route: ActivatedRoute, private _service:ServicesService) { }
 
   ngOnInit() {
-    this.routeSub = this.route.params.subscribe(params=>{
+    this.routeSub = this.route.params.subscribe((params) => {
       this.query = params['q']
-      this.req = this._service.search(this.query).subscribe(data=>{
-        this.searchList = data as [ServiceItem];
+      this.req = this._service.search(this.query).subscribe((data) => {
+        data = data.map((result) => {
+          if (result.type !== 'resource') {
+            result.slug = `${result.type}/${result.slug}`;
+          }
+
+          return result
+        })
+        this.searchList = data as [ServiceItem]
       })
     })
   }
 
-  ngOnDestroy(){
-    this.routeSub.unsubscribe();
-    this.req.unsubscribe();
+  ngOnDestroy() {
+    this.routeSub.unsubscribe()
+    this.req.unsubscribe()
   }
-
 }
