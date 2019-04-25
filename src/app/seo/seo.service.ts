70c0f401b48a04461ac793447934d7367a76d030
Max <max@maxleon.net>
2019-04-25 08:46:54 -0700

    - Restructure meta.json to allow multiple types of meta tags - Added support for <meta name="robots"> tag from  meta.json - Added ./thank-you/ to sitemap - Disallow ./thank-you/ with robots.txt - Set "noindex" on ./thank-you/ page

--- src/app/seo/seo.service.ts
+++ src/app/seo/seo.service.ts
@@ -10,7 +10,7 @@ import metaData from '../../assets/json/meta.json';
 export class SEOService {
   constructor(
     @Inject(DOCUMENT) private dom,
-    private meta: Meta,
+    private metaService: Meta,
     private titleService: Title
   ) {}
 
@@ -18,43 +18,54 @@ export class SEOService {
     this.updateCanonicalURL();
 
     // Set up defaults
-    let title = "Mike's Mobile Screen and Chimney Service";
-    let tag = {
+    let descriptionTag = {
       name: 'description',
       content:
         "Mike's Mobile Screen & Chimney offers Window Screen Repair, Security Screen Doors, Chimney inspections, Chimney Repairs, Retractable Awnings and more!"
     };
+    let robotsTag = {
+      name: 'robots',
+      content: 'index, follow'
+    };
+    let title = "Mike's Mobile Screen and Chimney Service";
+
+    const pageInfo = metaData.find((data) => data.page === url);
 
     // Overwrite defaults with found data
-    const metaInfo = metaData.find((data) => data.page === url);
-
-    if (metaInfo) {
-      if (metaInfo.content) {
-        tag.content = metaInfo.content;
+    if (pageInfo) {
+      if (pageInfo.description) {
+        descriptionTag.content = pageInfo.description;
       }
-      if (metaInfo.title) {
-        title = metaInfo.title;
+      if (pageInfo.robots) {
+        robotsTag.content = pageInfo.robots;
+      }
+      if (pageInfo.title) {
+        title = pageInfo.title;
       }
     }
 
+    this.metaService.updateTag(descriptionTag);
+    this.metaService.updateTag(robotsTag);
     this.titleService.setTitle(title);
-    this.meta.updateTag(tag);
   }
 
   updateCanonicalURL() {
-    // Update canonical link in DOM if possible
+    const canonicalURL = this.dom.URL;
+
     let existingCanonicalLink: HTMLLinkElement = this.dom.head.querySelector(
       "link[rel='canonical']"
     );
+
+    // Update canonical link in DOM if possible
     if (existingCanonicalLink) {
-      existingCanonicalLink.setAttribute('href', this.dom.URL);
+      existingCanonicalLink.setAttribute('href', canonicalURL);
       return;
     }
 
     // Generate new canonical link
     let newCanonicalLink: HTMLLinkElement = this.dom.createElement('link');
     newCanonicalLink.setAttribute('rel', 'canonical');
-    newCanonicalLink.setAttribute('href', this.dom.URL);
+    newCanonicalLink.setAttribute('href', canonicalURL);
     this.dom.head.appendChild(newCanonicalLink);
   }
 }
