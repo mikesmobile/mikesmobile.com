2a24ba70b5c37a60db84d6160169a9fd32cf8591
Max <max@maxleon.net>
2019-11-06 09:18:25 -0800

    - Added monthly cost calculator for 5 year lean

--- src/app/financing/financing.component.ts
+++ src/app/financing/financing.component.ts
@@ -5,4 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './financing.component.html',
   styleUrls: ['./financing.component.sass']
 })
-export class FinancingComponent {}
+export class FinancingComponent {
+  totalCostInput: number = 0;
+  monthlyPayment: string = '';
+
+  updateMonthly(event) {
+    const newMonthlyPayment: number = event * 0.0198;
+    if (isNaN(newMonthlyPayment)) {
+      this.monthlyPayment = '';
+    } else {
+      this.monthlyPayment = newMonthlyPayment.toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+      });
+    }
+  }
+}
