18ba9fd907a30a8582c5b842515773684d9db232
ZacWarner <zektgn@gmail.com>
2020-01-30 12:18:06 -0800

    changed promo page

--- src/app/promo/promo.component.ts
+++ src/app/promo/promo.component.ts
@@ -1,14 +1,36 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { QuoteFormPromoComponent } from '../quote-form-promo/quote-form-promo.component';
+import { JSONLDService } from '../services/jsonld.service';
+
+import servicesJSON from '../../assets/json/services.json';
+
 @Component({
   selector: 'app-promo',
   templateUrl: './promo.component.html',
   styleUrls: ['./promo.component.sass']
 })
-export class PromoComponent {
+export class PromoComponent implements OnInit {
+  service: any;
+
   @ViewChild(QuoteFormPromoComponent)
   private quoteForm: QuoteFormPromoComponent;
   openQuoteForm() {
     this.quoteForm.show();
+  }
+  constructor(private jsonService: JSONLDService) {}
+
+  ngOnInit() {
+    const promoService = 'chimney-cleaning';
+    this.service = servicesJSON.find((data) => {
+      if (data.slug === promoService) {
+        this.jsonService.updateJSONLD({
+          name: data.title,
+          description: data.tileText,
+          image: data.tileImage
+        });
+        return true;
+      }
+      return false;
+    });
   }
 }
