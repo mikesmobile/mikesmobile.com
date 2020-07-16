127152e708e97387e959f2b25b51b2d60ac7700d
ZacWarner <zektgn@gmail.com>
2020-07-16 15:43:27 -0700

    started branch for bullet points

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -232,6 +232,10 @@ const appRoutes: Routes = [
   { path: 'products/:slug', component: ProductDetailComponent },
   { path: 'services/:slug', component: ServiceDetailComponent },
   { path: 'services/masonry/:slug', component: ServiceDetailComponent },
+  {
+    path: 'services/chimney-bullet-proofing',
+    component: ServiceDetailComponent,
+  },
   // { path: 'promo', component: PromoComponent },
   { path: 'awningPriceCalc', component: AwningPriceCalcComponent },
   { path: 'dropRollPriceCalc', component: DropRollCalcComponent },
