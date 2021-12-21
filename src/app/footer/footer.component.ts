db11bdacf6a28c10307fdcc33b11b1cdf94535e4
christycreme95 <christyeang@yahoo.com>
2021-12-21 14:58:34 -0800

    a bit of edits

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -23,7 +23,7 @@ export class FooterComponent {
   
   footerSwitch() {
     // this is where I put the landing pages to remove the footer
-    if (this.currentRoute.startsWith('/about/gas-fireplace-service') || this.currentRoute.startsWith('/about/security-screen-doors') || this.currentRoute.startsWith('/grid/titans') || this.currentRoute.startsWith('/products/viewguards') || this.currentRoute.startsWith('/about/theWaterproofingPackage') || this.currentRoute.startsWith('/grid/our-chimney-repairs')) {
+    if (this.currentRoute.startsWith('/about/our-chimney-services') || this.currentRoute.startsWith('/about/gas-fireplace-service') || this.currentRoute.startsWith('/about/security-screen-doors') || this.currentRoute.startsWith('/grid/titans') || this.currentRoute.startsWith('/products/viewguards') || this.currentRoute.startsWith('/about/theWaterproofingPackage') || this.currentRoute.startsWith('/grid/our-chimney-repairs')) {
       this.turnMeOn = false;
     } else {
       this.turnMeOn = true;
