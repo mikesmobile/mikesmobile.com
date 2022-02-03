eaca8ee81c9a2ba7a54b56ffbe274701854295b2
christycreme95 <christyeang@yahoo.com>
2022-02-03 09:18:28 -0800

    transferring the wireframe to home page so eventually it can be changed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,22 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AnnouncementMessageComponent } from '../announcementMessage/announcementMessage.component';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.sass']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   @ViewChild(AnnouncementMessageComponent)
   private announcementMessage: AnnouncementMessageComponent;
+  ngOnInit(){
+    AOS.init({
+      once: true,
+      easing: 'ease-out-back',
+      duration: 700
+    });
+  }
 
   openAnnouncementMessage() {
     this.announcementMessage.show();
