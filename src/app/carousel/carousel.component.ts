8a8da768738c43090ff3701b4d7224366a59ea3b
christycreme95 <christyeang@yahoo.com>
2022-02-07 14:30:06 -0800

    carousel messing w stuff

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
@@ -6,7 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carousel.component.sass']
 })
 export class CarouselComponent implements OnInit {
-  
+  // images = [
+  //   {
+  //     "src": '../../assets/images/home/nofocus.png',
+  //     "alt": 'House without any security products'
+  //   },
+  //   {
+  //     "src": '../../assets/images/home/windowfocus.png',
+  //     "alt": 'Security Screens'
+  //   },
+  //   {
+  //     "src": '../../assets/images/home/doorfocus.png',
+  //     "alt": 'Viewguard Security Screen Door'
+  //   },
+  //   {
+  //     "src": '../../assets/images/home/chimfocus.png',
+  //     "alt": 'Chimney with a Mikes Mobile Cap and Waterproofing!'
+  //   }
+  // ]
+
   constructor() { }
 
   ngOnInit() {
